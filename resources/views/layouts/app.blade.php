5b3ec63f6b7701bf8c4c5270b707c6113d6ca9b9
Fiorezarn <fioreza1902@gmail.com>
2025-09-15 15:39:39 +0700

    fix:favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'CMS') }}</title>
+    <title>Cheil CMS</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <style>
         .navbar-brand {
